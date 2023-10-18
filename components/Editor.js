ebfbe655246446d0906b59588a2f84378a329316
Malik Kotb <malikkotb@icloud.com>
2023-10-18 12:35:35 +0200

    basic layout

---
+++ components/Editor.js
@@ -0,0 +1,6 @@
+
+export default function Editor() {
+  return (
+    <div className="bg-red-400">Editor</div>
+  )
+}
---
+++ components/Sidebar.js
@@ -0,0 +1,6 @@
+
+export default function Sidebar() {
+  return (
+    <div className="h-screen min-w-1/6">Sidebar</div>
+  )
+}
