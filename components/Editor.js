58687b0a8aef06d03f433da458ce19d6768a0725
Malik Kotb <malikkotb@icloud.com>
2023-10-18 14:50:11 +0200

    rich text editor

--- components/Editor.js
+++ components/Editor.js
@@ -1,3 +1,8 @@
+import Tiptap from "./Tiptap";
+
 export default function Editor() {
-  return <div className="w-7/12 bg-white p-4">Editor</div>;
+  return <div className="w-7/12 bg-white p-4">
+    <div>Name of Note</div>
+    <Tiptap />
+  </div>;
 }
