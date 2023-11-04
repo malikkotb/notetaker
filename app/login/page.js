8460f06dc94811936d8edd9cdb0fc35a89cee561
Malik Kotb <malikkotb@icloud.com>
2023-11-04 20:09:18 +0100

    login page

---
+++ app/login/page.js
@@ -0,0 +1,10 @@
+import { CreateAccount } from "../../components/CreateAccount";
+
+export default function page() {
+  return (
+    <>
+      <div>Login Page</div>
+      <CreateAccount />
+    </>
+  );
+}
