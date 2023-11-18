0736b7813becfa95331cbe5cdeac8dfa7a18a5b5
Malik Kotb <malikkotb@icloud.com>
2023-11-18 14:46:30 +0100

    create account page

--- components/authentication/Login.js
+++ components/authentication/Login.js
@@ -1,29 +1,33 @@
-// import { CreateAccount } from "./CreateAccount";
+import { CreateAccount } from "../CreateAccount";
 
 import { PiNotebookLight } from "react-icons/pi";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "../ui/button";
 import { UserAuthForm } from "./User-Auth-Form";
 import { ModeToggle } from "../ModeToggle";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "../ui/button";
+import { useState } from "react";
 
 export default function AuthenticationPage() {
+
+  const [create, setCreate] = useState(true)
+
   return (
     <>
       <div className="container relative hidden h-screen flex-col items-center justify-center sm:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="absolute flex right-4 top-4 md:right-8 md:top-8">
-          {/* <Link
+        <div className="absolute flex right-4 gap-1 top-4 md:right-8 md:top-8">
+          <Link
             href="/"
+            onClick={() => setCreate(!create)}
             className={cn(
               buttonVariants({ variant: "ghost" })
               // "absolute right-4 top-4 md:right-8 md:top-8"
             )}
           >
-            Login
-          </Link> */}
+            {create ? "Login" : "Create an account"}
+          </Link>
           <ModeToggle />
         </div>
-
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 gap-2 flex items-center text-lg font-medium">
@@ -46,25 +50,25 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                {create ? "Create an account" : "Login"}
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+              <p className="text-sm text-zinc-400">
+                Enter your email and password below to {create ? "create an account" : "login"}
               </p>
             </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
+            {create ? (<CreateAccount />) : (<UserAuthForm />)}
+            <p className="px-8 text-center text-sm text-zinc-400">
               By clicking continue, you agree to our{" "}
               <Link
                 href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
+                className="underline underline-offset-4 hover:text-zinc-600"
               >
                 Terms of Service
               </Link>{" "}
               and{" "}
               <Link
                 href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
+                className="underline underline-offset-4 hover:text-zinc-600"
               >
                 Privacy Policy
               </Link>
---
+++ components/test.js
@@ -0,0 +1,136 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { useEffect, useState } from "react";
+import pb from "../../app/(lib)/pocketbase";
+import { useForm } from "react-hook-form";
+import useLogout from "../../app/(hooks)/useLogout";
+import useLogin from "../../app/(hooks)/useLogin";
+import { Toaster, toast } from "sonner";
+import useMyStore from "../../app/(store)/store";
+import { GitHub } from "../icons/GitHub";
+import { Spinner } from "../icons/Spinner";
+import { RiGoogleLine } from "react-icons/ri";
+
+export function UserAuthForm({ className, ...props }) {
+  const logout = useLogout();
+  const { mutate, isLoading, isError, error, isSuccess } = useLogin();
+  const { register, handleSubmit, reset } = useForm();
+  const { authenticated, toggleAuthenticated } = useMyStore();
+  useEffect(() => {
+    if (isSuccess) {
+      toggleAuthenticated();
+    } else if (isError) {
+      toast.error("Login failed");
+    }
+  }, [isSuccess, isError]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Client-side code here
+      logout();
+      console.log("logout on Mount");
+    }
+  }, []);
+
+  // const isLoggedIn = pb.authStore.isValid;
+
+  async function onSubmit(data) {
+    mutate({ email: data.email, password: data.password }); // mutate is basically the login function in the useLogin hook
+    reset();
+  }
+
+  // There is an error (hydration error) if you login successfully and then refresh the page
+  // this occurs because you will be still logged in in the backend
+  // but the jsx that is rendered is the one for when the user is not logged in
+
+  // Initial State on the Server: If you're fetching data during the initial rendering, make
+  // sure that the server sends the same data that the client expects.
+  // Mismatched data can cause hydration issues.
+
+  // if (isLoggedIn) {
+  //   return (
+  //     <div>
+  //       <Toaster position="bottom-right" richColors />
+  //       <h1 suppressHydrationWarning>Logged In: {pb.authStore.model.email}</h1>
+  //       <button onClick={() => logout()}>Logout</button>
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <div>
+      <Toaster position="bottom-right" richColors />
+      <div className={cn("grid gap-6", className)} {...props}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-2">
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+                {...register("email")}
+              />
+              <Input
+                id="password"
+                placeholder="Password"
+                type="password"
+                disabled={isLoading}
+                {...register("password")}
+              />
+            </div>
+            {isError && (
+              <p className="text-xs text-red-500">
+                Invalid email or password. Error: {error.message}
+              </p>
+            )}
+            <Button disabled={isLoading}>
+              {isLoading && <Spinner className="mr-2 h-4 w-4 animate-spin" />}
+              Sign In with Email
+            </Button>
+          </div>
+        </form>
+
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-white dark:bg-black px-2">
+              Or continue with
+            </span>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-6">
+          <Button variant="outline" type="button" disabled={isLoading}>
+            {isLoading ? (
+              <Spinner className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <GitHub className="mr-2 h-4 w-4" />
+            )}
+            Github
+          </Button>
+
+          <Button variant="outline" type="button" disabled={isLoading}>
+            {isLoading ? (
+              <Spinner className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <RiGoogleLine className="mr-2 h-4 w-4" />
+            )}
+            Google
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
