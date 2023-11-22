cf26dcd86da811a88f55e22b45d2675d9308fdfc
Malik Kotb <malikkotb@icloud.com>
2023-11-22 19:02:32 +0100

    loading screen

--- app/loading.js
+++ app/loading.js
@@ -1,6 +1,9 @@
+import ThreeDotsWave from "@/components/ThreeDotsAnimation";
 
 export default function loading() {
   return (
-    <div>loading.....</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <ThreeDotsWave />
+    </div>
+  );
 }
